c0865d24b40a35531c46b485bc681558b5f46bd1
Ayush <ayushkarma.dev@gmail.com>
2026-01-30 14:21:14 +0530

    Update serverurl in frontend

--- stash-frontend/src/pages/constents.js
+++ stash-frontend/src/pages/constents.js
@@ -1,2 +1,2 @@
-// export const serverUrl = "http://localhost:8000/api/v1/";
-export const serverUrl = "http://192.168.1.3:8000/api/v1/";
+// export const serverUrl = "http://192.168.1.3:8000/api/v1/";
+export const serverUrl = "https://stash-backend-fel8.onrender.com/api/v1/";
